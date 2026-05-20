668be18ad5e085ee4ed7542e780ed4448722a70d
bisieshvili <mishobisieshvili@yahoo.com>
2026-05-20 18:32:07 +0400

    initial commit

---
+++ Homework1/Program.cs
@@ -0,0 +1,18 @@
+﻿ using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.BackgroundColor = ConsoleColor.Blue;
+        Console.Clear();
+
+       
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write("misho bisieshvili ");
+
+        string input = Console.ReadLine();
+
+        Console.WriteLine(input);
+    }
+}
